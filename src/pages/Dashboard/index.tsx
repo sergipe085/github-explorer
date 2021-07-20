65a772c6220695ab2cfb7db0d72654acc21a0a82
Sérgio <71034184+serjolas1@users.noreply.github.com>
2021-07-20 15:30:58 -0300

    dashboard page styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,51 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi"
 
-import { Title } from "./styles";
+import logoImg from "../../assets/logo.svg"
+
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
     return (
-        <Title>Explore repositorios no Github</Title>
+        <>
+            <img src={logoImg} alt="Github Explorer"></img>
+            <Title>Explore repositorios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositorio"/>
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img src="https://github.com/serjolas1.png" alt="Serjolas1"/>
+                    <div>
+                        <strong>serjolas1/letmeask</strong>
+                        <p>Letmeask é perfeito para pessoas poderem criar salas de pergunta e resposta com o seu público, de uma forma muito organizada e democrática.</p>
+                    </div>
+
+                    <FiChevronRight size={60}/>
+                </a>
+                <a href="teste">
+                    <img src="https://github.com/serjolas1.png" alt="Serjolas1"/>
+                    <div>
+                        <strong>serjolas1/letmeask</strong>
+                        <p>Letmeask é perfeito para pessoas poderem criar salas de pergunta e resposta com o seu público, de uma forma muito organizada e democrática.</p>
+                    </div>
+
+                    <FiChevronRight size={60}/>
+                </a>
+                <a href="teste">
+                    <img src="https://github.com/serjolas1.png" alt="Serjolas1"/>
+                    <div>
+                        <strong>serjolas1/letmeask</strong>
+                        <p>Letmeask é perfeito para pessoas poderem criar salas de pergunta e resposta com o seu público, de uma forma muito organizada e democrática.</p>
+                    </div>
+
+                    <FiChevronRight size={60}/>
+                </a>
+            </Repositories>
+        </>
     );
 }
 
